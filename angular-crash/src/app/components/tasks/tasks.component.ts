dcfb0423b155ae0fd5c1b07542f8612f339a7e2a
Miguel <menjivarmiguel221@gmail.com>
2021-10-05 20:45:05 -0700

    deleting tasks feature and adding a dummy json server

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskService } from '../../services/task.service'
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
-
 
 @Component({
   selector: 'app-tasks',
@@ -15,5 +14,13 @@ export class TasksComponent implements OnInit {
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
   }
 }
